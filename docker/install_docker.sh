7f41fb24e2903b65718d3d210873e23cfd462863
PhungQuan-business <phunghongquan.business@gmail.com>
2025-06-19 15:53:00 +0700

    command to apply changes to docker group

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -11,8 +11,8 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
@@ -22,6 +22,9 @@ sudo groupadd docker
 
 # Add your user to the docker group
 sudo usermod -aG docker $USER
+
+#  Please log out and log back in (or run 'newgrp docker') for changes to take effect."
+newgrp docker
 
 # Inform user to log out and log back in for changes to take effect
 echo "You must log out and log back in for Docker to run without sudo. After logging back in, you can verify by running 'docker ps'."
